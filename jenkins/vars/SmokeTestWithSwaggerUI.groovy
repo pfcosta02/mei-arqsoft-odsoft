6e0fdeba70a7af4d6976aff9cc4088957cf4919a
Diogo Pereira <1221137@isep.ipp.pt>
2026-01-04 22:16:47 +0000

    Lendings_C - Smoke Test

--- jenkins/vars/SmokeTestWithSwaggerUI.groovy
+++ jenkins/vars/SmokeTestWithSwaggerUI.groovy
@@ -1,11 +1,12 @@
+import org.jenkinsPipeline.Constants
+
 def call(String serviceName, String namespace, int port) {
 
-    // serviceName aqui deve ser o NOME EXATO do Service, por ex. "lendings-c-service"
     def url = "http://${serviceName}.${namespace}.svc.cluster.local:${port}/swagger-ui/index.html"
 
     echo """
         ==========================================
-        GENERIC SMOKE TEST
+        IN-CLUSTER SMOKE TEST (Swagger UI)
         ==========================================
         Service:   ${serviceName}
         Namespace: ${namespace}
@@ -13,27 +14,36 @@ def call(String serviceName, String namespace, int port) {
         ==========================================
     """
 
-    try {
-        if (isUnix()) {
-            sh """
-                set -e
-                echo "Calling ${url} ..."
-                curl -f -s -o /dev/null "${url}"
-            """
-        } else {
-            bat """
-                echo Calling ${url} ...
-                curl -f -s -o NUL "${url}"
-                if %ERRORLEVEL% NEQ 0 (
-                    echo Smoke test FAILED for ${url}
-                    exit /b 1
-                )
-            """
-        }
+    withCredentials([
+            file(
+                    credentialsId: Constants.ENVIRONMENT_2_CREDENTIALS_ID[namespace],
+                    variable: 'KUBECONFIG'
+            )
+    ]) {
+        try {
+            if (isUnix()) {
+                sh """
+                    set -e
+                    # criar pod temporário com curl dentro do cluster
+                    kubectl run curl-smoke --rm -i --restart=Never --image=curlimages/curl -n ${namespace} -- \
+                      sh -c 'echo "Calling ${url} ..." && curl -f -s -o /dev/null "${url}"'
+                """
+            } else {
+                bat """
+                    echo Creating in-cluster curl pod for smoke test...
+                    kubectl run curl-smoke --rm -i --restart=Never --image=curlimages/curl -n ${namespace} -- ^
+                      sh -c "echo Calling ${url} ... && curl -f -s -o /dev/null \\"${url}\\""
+                    if %ERRORLEVEL% NEQ 0 (
+                        echo Smoke test FAILED for ${url}
+                        exit /b 1
+                    )
+                """
+            }
 
-        echo "✅ Smoke test PASSED for ${url}"
-    } catch (Exception e) {
-        echo "❌ Smoke test FAILED for ${url}: ${e.getMessage()}"
-        throw e
+            echo "✅ Smoke test PASSED for ${url}"
+        } catch (Exception e) {
+            echo "❌ Smoke test FAILED for ${url}: ${e.getMessage()}"
+            throw e
+        }
     }
 }

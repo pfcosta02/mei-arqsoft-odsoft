9a6030b3c6ec68dd399ab9c9b1f49b3957765d87
Tiago <1201360@isep.ipp.pt>
2026-01-04 12:15:24 +0000

    Minor changes to k6 file

--- Docs/ARQSOFT-Docs/Part2/k6_loadTest_GET.js
+++ Docs/ARQSOFT-Docs/Part2/k6_loadTest_GET.js
@@ -17,12 +17,6 @@ export let options = {
     ],
 };
 
-// export function handleSummary(data) {
-//     return {
-//         "/tests/summary_k6_loadTest_GET_Store.html": htmlReport(data),
-//     };
-// }
-
 // Login once before the test starts
 export function setup() {
     const loginRes = http.post('http://localhost:8089/api/public/login', JSON.stringify({
@@ -38,47 +32,9 @@ export function setup() {
 
 // Main test function
 export default function () {
-    // Create: Adding a new book
-    // let createPayload = JSON.stringify({
-    //     authors: [{ firstName: "John" + Math.random(), lastName: "Doe" }],
-    //     description: "desc",
-    //     isbn: "123-1234567890" + Math.random(),
-    //     publisher: "My publisher",
-    //     title: "My book"
-    // });
 
-    // let createRes = http.post('http://rest-books:8080/api/books', createPayload, { headers: { 'Content-Type': 'application/json', 'Accept': '*/*' } });
-    // check(createRes, { 'created book': (r) => r.status === 201 });
-
-    // // Get: Fetching a specific book
-    // let getRes = http.get('http://rest-books:8080/api/books/978-0321356680');
-    // check(getRes, { 'retrieved book': (r) => r.status === 200 });
-
-    // // Update: Updating book info using data from the CSV
-    // const book = csvData[Math.floor(Math.random() * csvData.length)];
-    // let updatePayload = JSON.stringify({
-    //     authors: [{ firstName: book.author_first_name, lastName: book.author_last_name }],
-    //     description: book.description,
-    //     isbn: book.isbn,
-    //     publisher: book.publisher,
-    //     title: book.title
-    // });
-
-    // let updateRes = http.put('http://rest-books:8080/api/books/${book.isbn}', updatePayload, { headers: { 'Content-Type': 'application/json' } });
-    // check(updateRes, { 'updated book': (r) => r.status === 200 });
-
-    // // Patch: Updating the book's description
-    // let patchPayload = JSON.stringify("new description");
-
-    // let patchRes = http.patch('http://rest-books:8080/api/books/978-1491900864', patchPayload, { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' } });
-    // check(patchRes, { 'patched book description': (r) => r.status === 200 });
-
-    // // Delete: Removing a book
-    // let deleteRes = http.del('http://rest-books:8080/api/books/978-1617292545');
-    // check(deleteRes, { 'deleted book': (r) => r.status === 204 });
-
-    // Get All: Fetching all books
-    let getAuthor = http.get('http://localhost:8083/api/authors?Robert');
+    // Get: Fetching author
+    let getAuthor = http.get('http://localhost:8083/api/authors?Ant');
     check(getAuthor, { 'retrieved author': (r) => r.status >= 200 && r.status < 300 });
 
     sleep(1);
